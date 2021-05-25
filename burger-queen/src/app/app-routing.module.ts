d2abf95d3f4ec2223a418dd7e000e6992df370de
mairelis-montilla <mairelismontilla@gmail.com>
2021-05-25 12:41:38 -0500

    feat routing components

--- burger-queen/src/app/app-routing.module.ts
+++ burger-queen/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { WaiterComponent } from './waiter/waiter.component';
+import { AdminComponent } from './admin/admin.component';
+import { ChefComponent } from './chef/chef.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+},
+{
+  path: 'admin',
+  component: AdminComponent,
+},
+{
+  path: 'waiter',
+  component: WaiterComponent,
+},{
+  path: 'chef',
+  component: ChefComponent,
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
