714deeef7cd2142b3185db67634f32b8652843f5
mairelis-montilla <mairelismontilla@gmail.com>
2021-05-28 18:57:02 -0500

    feat get token to login(login.component)

--- burger-queen/src/app/login/login.component.ts
+++ burger-queen/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DataApiService} from 'src/app/services/data-api.service';
+import { DataApiService } from 'src/app/services/data-api.service';
 
 @Component({
   selector: 'app-login',
@@ -8,18 +8,23 @@ import { DataApiService} from 'src/app/services/data-api.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private dataApiService: DataApiService){}
+  constructor(private dataApiService: DataApiService) { }
 
   ngOnInit() {
     this.getlist();
   }
 
-  getlist(){
-  this.dataApiService.getUserById('http://localhost:3200/', 'users/', '001').subscribe(data => console.log(data));}
+  getlist() {
+    this.dataApiService.getById('http://localhost:3200/', 'users/', '001').subscribe(data => console.log(data));
+  }
 
-  login(email: any, password: any, rol: any){
-    console.log(email.value, password.value, rol.value)
-    return false
+  login(email: any, password: any) {
+    const body = {
+      email: email.value,
+      password: password.value
+    }
+    return this.dataApiService.loginAuth('http://localhost:3200/', 'auth', body).subscribe(data => console.log(data));
+
   }
 
 }
