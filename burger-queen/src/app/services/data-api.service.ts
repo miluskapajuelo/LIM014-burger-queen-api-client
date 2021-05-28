c582bf8db01d43adb513328477c9a2cacaee1677
mairelis-montilla <mairelismontilla@gmail.com>
2021-05-28 18:56:00 -0500

    feat services put, delete and post data(data-api)

--- burger-queen/src/app/services/data-api.service.ts
+++ burger-queen/src/app/services/data-api.service.ts
@@ -7,14 +7,27 @@ import { HttpClient } from '@angular/common/http'
 })
 export class DataApiService {
 
-  constructor( private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient) {
     console.log('is working http')
   }
 
-    getUserById(url:any,hash:any, uid:any){
-      return this.httpClient.get(`${url}${hash}${uid}`);
-   }
-    getAll(url:any,hash:any){
+  getAll(url: any, hash: any) {
     return this.httpClient.get(`${url}${hash}`);
- }
+  }
+  getById(url: any, hash: any, uid: any) {
+    return this.httpClient.get(`${url}${hash}${uid}`);
+  }
+  deleteById(url: any, hash: any, uid: any) {
+    return this.httpClient.delete(`${url}${hash}${uid}`)
+  }
+  updateById(url: any, hash: any, uid: any, body: any) {
+    return this.httpClient.put(`${url}${hash}${uid}`, body)
+  }
+  create(url: any, hash: any,body: any){
+    return this.httpClient.post(`${url}${hash}`, body)
+  }
+
+  loginAuth(url: any, hash: any, body: any) {
+    return this.httpClient.post(`${url}${hash}`, body)
+  }
 }
