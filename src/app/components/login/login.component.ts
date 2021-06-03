70d87b5af2723659c05f730f2f856788d126bd15
mairelis-montilla <mairelismontilla@gmail.com>
2021-06-03 01:13:10 -0500

    feat error message

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthApiService } from 'src/app/services/auth-api.service';
 import jwt_decode from "jwt-decode";
 import { Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +11,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
-
   constructor(private authApiService: AuthApiService, private router: Router) { }
+  error: boolean = false;
+  errorMessage: string = 'Incorret Email or Password';
 
   ngOnInit() {
   }
@@ -22,12 +25,31 @@ export class LoginComponent implements OnInit {
     }
 
     this.authApiService.loginAuth(body)
+      .pipe(
+        catchError((error) => {
+
+          console.log('error', error);
+
+          if (error.status === 400) {
+            this.error = true;
+            console.log('error de credenciales');
+          }
+          return throwError(error);
+        })
+      )
       .subscribe((data: any) => {
-        console.log(data)
+        this.error = false;
         window.localStorage.setItem('token', data.resp.token);
-        const token = localStorage.getItem('token');
-        console.log('objeto decode', jwt_decode(data.resp.token));
-        this.router.navigate(['admin']);
+        const token: any = jwt_decode(data.resp.token);
+        if (token.rol === 'admi') {
+          this.router.navigate(['admin']);
+        }
+        else if (token.rol === 'chef') {
+          this.router.navigate(['chef']);
+        } else {
+          this.router.navigate(['waiter']);
+        }
+
 
       });
 
