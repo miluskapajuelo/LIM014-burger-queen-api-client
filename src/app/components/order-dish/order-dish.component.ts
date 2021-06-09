68b5e1564ac831bbf1813fcb4077bcc9a8b579ec
mairelis-montilla <mairelismontilla@gmail.com>
2021-06-09 15:08:46 -0500

    fix order-dish component inputs and outputs

--- src/app/components/order-dish/order-dish.component.ts
+++ src/app/components/order-dish/order-dish.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ProductDetailModel } from 'src/app/models/products-model';
-import { IOrderModel } from 'src/app/models/orders-model';
+import { OrderProductModel } from 'src/app/models/orders-model';
 
 @Component({
   selector: 'app-order-dish',
@@ -10,39 +10,26 @@ import { IOrderModel } from 'src/app/models/orders-model';
 export class OrderDishComponent implements OnInit {
 
 
-  index:number
-  producSelected: any
-
-  @Input()selectProduct:Array<ProductDetailModel>=[];
-  @Input()productitem:Array<IOrderModel>
-  @Input() objet:any
-  @Output() deleteItem: EventEmitter<ProductDetailModel> = new EventEmitter()
+  @Input() productitem: Array<OrderProductModel>
+  @Output() deleteItem: EventEmitter<OrderProductModel> = new EventEmitter()
+  @Output() addItem: EventEmitter<OrderProductModel> = new EventEmitter()
+  @Output() removeItem: EventEmitter<OrderProductModel> = new EventEmitter()
 
   constructor() {
-    this.index=0
-    this.productitem=[]
+    this.productitem = []
   }
 
   ngOnInit(): void {
   }
 
-  deleteProduct(item:ProductDetailModel){
+  deleteProduct(item: OrderProductModel) {
     this.deleteItem.emit(item)
   }
-
-  addItem(item:any){
-
-    this.objet.quantity += 1
-
-   /*  this.productitem.push(this.objet)
-    console.log('arrayobje',this.objet) */
-
-    /* this.productitem.forEach(element => {element._id == item._id}) */
+  addProduct(item: OrderProductModel) {
+    this.addItem.emit(item)
   }
-
-  removeItem(item:any){
-    this.index=this.productitem.indexOf(item)
-    this.productitem.splice(this.index,1)
+  removeProduct(item: OrderProductModel) {
+    this.removeItem.emit(item)
   }
 
 }
