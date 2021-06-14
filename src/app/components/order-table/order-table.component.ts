3864309dec667b5d423e57695d65ad5f9bb77cfc
mairelis-montilla <mairelismontilla@gmail.com>
2021-06-13 22:48:25 -0500

    add update status order

--- src/app/components/order-table/order-table.component.ts
+++ src/app/components/order-table/order-table.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit, Input
+import {
+  Component, OnInit, Input
 } from '@angular/core';
-import { IOrderModel
+import {
+  IOrderModel
 } from 'src/app/models/orders-model';
+import { OrderApiService } from '../../services/order-api.service'
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
 
 @Component({
   selector: 'app-order-table',
@@ -10,8 +16,52 @@ import { IOrderModel
   ]
 })
 export class OrderTableComponent implements OnInit {
-  @Input()item:IOrderModel ={} as IOrderModel;
-  constructor() {}
+  @Input() item: IOrderModel = {} as IOrderModel;
+  constructor(private orderApiService: OrderApiService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
+
+  update(item: any) {
+    const date = new Date().toLocaleDateString('es-Es');
+    const time = new Date().toLocaleTimeString('es-Es');
+    if (item.status === 'pending') {
+      const order: IOrderModel = {
+        ...item,
+        status: 'delivering',
+        dateprocesed: `${date} ${time}`
+      }
+      console.log(order)
+      this.orderApiService.updateOrder(item._id, order).pipe(
+        catchError((error) => {
+          console.log('error', error);
+          if (error.status === 400) {
+            console.log('error de credenciales');
+          }
+          return throwError(error);
+        })
+      ).subscribe((data: any) => {
+        console.log('pending', data)
+      })
+    }
+    else if (item.status === 'delivering') {
+      const order: IOrderModel = {
+      ...item,
+        status: 'ready',
+        dateprocesed: `${date} ${time}`
+      }
+      console.log(order)
+      this.orderApiService.updateOrder(item._id, order).pipe(
+        catchError((error) => {
+          console.log('error', error);
+          if (error.status === 400) {
+            console.log('error de credenciales');
+          }
+          return throwError(error);
+        })
+      ).subscribe((data: any) => {
+        console.log('delivering', data)
+      })
+    }
+
+  }
 }
