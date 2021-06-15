4332da3f2228373c8ee756f23818b6dfcff4ad91
mairelis-montilla <mairelismontilla@gmail.com>
2021-06-14 21:12:45 -0500

    feat getAllProduts

--- src/app/components/manage/manage.component.ts
+++ src/app/components/manage/manage.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IProductsModel, ProductDetailModel } from 'src/app/models/products-model';
+import { IUserModel, UserDetailModel } from 'src/app/models/user-model';
+import { ProductsApiService } from 'src/app/services/products-api.service';
+import { UserApiService } from 'src/app/services/user-api.service';
 
 @Component({
   selector: 'app-manage',
@@ -9,11 +13,29 @@ import { Component, OnInit } from '@angular/core';
 
 export class ManageComponent implements OnInit {
   clicked: boolean;
-  constructor() {
+  products: Array<ProductDetailModel>
+  users: Array<UserDetailModel>
+  constructor(private productsApiService: ProductsApiService, private userApiService: UserApiService) {
     this.clicked = true;
   }
 
   ngOnInit(): void {
+    this.getProducts()
+    this.getUsers()
+  }
+  getProducts() {
+    this.productsApiService.getAllProducts().subscribe((data: IProductsModel) => {
+      console.log(data)
+      this.products = data.products
+
+    })
+  }
+  getUsers() {
+    this.userApiService.getAllUsers().subscribe((data: IUserModel) => {
+      this.users = data.user
+      console.log(this.users)
+    })
+
   }
   changeFalse() {
     this.clicked = false
