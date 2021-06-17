d9884511dd51b9d43a9b023a150ea6cc2f9df86c
mairelis-montilla <mairelismontilla@gmail.com>
2021-06-17 11:49:55 -0500

    fix communication in components manage and pop

--- src/app/components/manage-products-table/manage-products-table.component.ts
+++ src/app/components/manage-products-table/manage-products-table.component.ts
@@ -8,38 +8,33 @@ import { ProductDetailModel } from 'src/app/models/products-model';
   styleUrls: ['./manage-products-table.component.sass']
 })
 export class ManageProductsTableComponent implements OnInit {
-  active:boolean
   filterValue: string
   clicked: boolean;
   @Input() products: Array<ProductDetailModel> = []
   @Output() deleteProductById: EventEmitter<any> = new EventEmitter()
   @Output() updatePorductById: EventEmitter<any> = new EventEmitter()
+  @Output() createProducts: EventEmitter<any> = new EventEmitter()
 
   constructor() {
-    this.filterValue=''
-    this.clicked=false
-    this.active=false
-   }
+    this.filterValue = ''
+    this.clicked = false
+  }
 
   ngOnInit(): void {
   }
-  deleteProduct(product:any){
+  deleteProduct(product: any) {
     this.deleteProductById.emit(product)
     this.clicked = false
   }
-  updatePorduct(product:any){
+  updatePorduct(product: any) {
     this.updatePorductById.emit(product)
     this.clicked = true
   }
-  createProduct(){
-    this.active = true
+  createProduct() {
+    this.createProducts.emit()
   }
-  handleSearch(value:string){
-    this.filterValue= value
-  }
-  closeModal(){
-    console.log("hola aqui estoy")
-    this.active = false
+  handleSearch(value: string) {
+    this.filterValue = value
   }
 
 }
