6bdd451bf31bf912524206d869be7da69d84a79f
mairelis-montilla <mairelismontilla@gmail.com>
2021-06-23 21:31:43 -0500

    refactor status orders

--- src/app/components/statusOrders/statusOrders.component.ts
+++ src/app/components/statusOrders/statusOrders.component.ts
@@ -2,6 +2,8 @@ import {
   Component, OnInit
 } from '@angular/core';
 import dayjs from 'dayjs';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import {
   IOrderModel
 } from 'src/app/models/orders-model';
@@ -23,27 +25,26 @@ export class StatusOrdersComponent implements OnInit {
 
   constructor(private OrderApiService: OrderApiService) {
     this.clicked = true;
-    this.items = [];
     this.itemDelivering = [];
     this.itemPending = [];
   }
 
   ngOnInit(): void {
-    this.OrderApiService.getAllOrders().subscribe((data) => {
-      this.items = data.orders
-      this.itemPending = data.orders.filter((elem) => elem.status === 'pending')
-      this.itemDelivering = data.orders.filter((elem) => elem.status === 'delivering')
-      console.log('pending ordered', this.itemPending.sort((a:IOrderModel, b:IOrderModel) => {
-        let formatA:any = dayjs(a.dateEntry).format('HHmmss')
-        let formatB:any = dayjs(b.dateEntry).format('HHmmss')
-        return formatA - formatB;
-    }))
-    console.log('delivering ordered', this.itemDelivering.sort((a:IOrderModel, b:IOrderModel) => {
-      let formatA:any = dayjs(a.dateEntry).format('HHmmss')
-      let formatB:any = dayjs(b.dateEntry).format('HHmmss')
-      return formatB - formatA;
+    this.getOrders()
+  }
 
-  }))
+  getOrders() {
+    this.OrderApiService.getAllOrders().subscribe((data) => {
+      this.itemPending = data.orders.filter((elem) => elem.status === 'pending').sort((a: IOrderModel, b: IOrderModel) => {
+        let formatA: any = dayjs(a.dateEntry).format('HHmmss')
+        let formatB: any = dayjs(b.dateEntry).format('HHmmss')
+        return formatA - formatB;
+      })
+      this.itemDelivering = data.orders.filter((elem) => elem.status === 'delivering').sort((a: IOrderModel, b: IOrderModel) => {
+        let formatA: any = dayjs(a.dateEntry).format('HHmmss')
+        let formatB: any = dayjs(b.dateEntry).format('HHmmss')
+        return formatA - formatB;
+      })
     });
   }
   changeFalse() {
@@ -51,5 +52,43 @@ export class StatusOrdersComponent implements OnInit {
   }
   changeTrue() {
     this.clicked = true
+  }
+  updateOrders(item: any) {
+    const dateProcesed = dayjs();
+    if (item.status === 'pending') {
+      const order: IOrderModel = {
+        ...item,
+        status: 'delivering',
+        dateProcesed: dateProcesed.format('YYYY-MM-DD HH:mm:ss')
+      }
+      this.OrderApiService.updateOrder(item._id, order).pipe(
+        catchError((error) => {
+          if (error.status === 400) {
+            alert('Opss something is wrong, try again!');
+          }
+          return throwError(error);
+        })
+      ).subscribe(() => {
+        this.getOrders()
+      })
+    }
+    else if (item.status === 'delivering') {
+      const order: IOrderModel = {
+        ...item,
+        status: 'ready',
+        dateProcesed: dateProcesed.format('YYYY-MM-DD HH:mm:ss')
+      }
+      this.OrderApiService.updateOrder(item._id, order).pipe(
+        catchError((error) => {
+          if (error.status === 400) {
+            alert('Opss something is wrong, try again!');
+          }
+          return throwError(error);
+        })
+      ).subscribe(() => {
+        this.getOrders()
+      })
+    }
+
   }
 }
