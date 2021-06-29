fc683d77c096bb6850e10a8e6baefc4a96e055d9
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-06-29 12:04:18 -0500

    feat(forms): add reactive forms in manage tables

--- src/app/components/users-pop/users-pop.component.ts
+++ src/app/components/users-pop/users-pop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-users-pop',
@@ -10,9 +11,22 @@ export class UsersPopComponent implements OnInit {
 
   @Output() closeModalUser: EventEmitter<any> = new EventEmitter()
   @Output() createUser: EventEmitter<{}> = new EventEmitter()
+  admin:Array<String>
 
 
-  constructor() { }
+  constructor() {
+
+  this.admin = ['yes', 'no'];
+  }
+
+
+  myForm = new FormGroup({
+    email: new FormControl('',Validators.required),
+    password: new FormControl('',Validators.required),
+    rol: new FormControl('',Validators.required),
+  })
+
+
 
   ngOnInit(): void {
   }
@@ -22,11 +36,12 @@ export class UsersPopComponent implements OnInit {
     this.closeModalUser.emit()
   }
 
-  //create user
-  createUsers(user:HTMLInputElement, price:HTMLInputElement){
-    const userNew ={ user: user.value, price:price.value}
-      this.createUser.emit(userNew)
-    }
-
+  //submit form
+   onSubmit() {
+    const userNew=this.myForm.value;
+    this.createUser.emit(userNew)
+    this.myForm.reset()
+    this.closeModalUsers()
+  }
 
 }
