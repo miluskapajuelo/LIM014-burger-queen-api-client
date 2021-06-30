3e96605d0f9ab65316a9885a98012cabf534aabf
mairelis-montilla <mairelismontilla@gmail.com>
2021-06-30 18:53:13 -0500

    add for to labels

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -50,6 +50,7 @@ export class LoginComponent implements OnInit {
           this.error = false;
           window.localStorage.setItem('token', data.token);
           const token: any = jwt_decode(data.token);
+          console.log('test')
           if (token.roles.admin) {
             this.router.navigate(['manage']);
           } else {
