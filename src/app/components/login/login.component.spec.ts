f15999cdfc2e2c7b28f2fadaa712a0b402a2d648
mairelis-montilla <mairelismontilla@gmail.com>
2021-07-05 07:52:26 -0500

    feat test to errors

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -7,7 +7,8 @@ import { Router } from '@angular/router';
 import { ModuleTesting } from 'src/app/testing-module';
 
 import { LoginComponent } from './login.component';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -22,7 +23,9 @@ describe('LoginComponent', () => {
       declarations: [
         LoginComponent,
         ...ModuleTesting
-      ], providers: [
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
         { provide: Router, useValue: mockRouter },
         { provide: AuthApiService }
       ],
@@ -45,7 +48,7 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  it('Should be login', async () => {
+  it('Should be login navigate manage', () => {
     const response = {
       "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MGM5MmMxM2NjNTMzNTBmMzkwNDk5ZGQiLCJlbWFpbCI6ImFkbWluQGxvY2FsaG9zdCIsInJvbGVzIjp7ImFkbWluIjp0cnVlfSwiaWF0IjoxNjIzNzk2ODg0LCJleHAiOjE2MjM4MDA0ODR9.8tDX3i4afFU7TAFfvg11ngvIJOYgZZ2oQDpLu0xYrpg"
     }
@@ -57,9 +60,37 @@ describe('LoginComponent', () => {
     spyOn(authApiService, 'loginAuth').and.returnValue(of(response))
     component.login()
     fixture.detectChanges();
-    fixture.whenStable().then(() => {
-    });
+    fixture.whenStable();
     expect(mockRouter.navigate).toHaveBeenCalled()
+    expect(authApiService.loginAuth).toHaveBeenCalled()
+
+  })
+  it('Should be login navigate waiter', () => {
+    const response = {
+      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MGM5MmMxM2NjNTMzNTBmMzkwNDk5ZGQiLCJlbWFpbCI6ImFkbWluQGxvY2FsaG9zdCIsInJvbGVzIjp7ImFkbWluIjpmYWxzZX0sImlhdCI6MTYyMzc5Njg4NCwiZXhwIjoxNjIzODAwNDg0fQ.0jjMrsFCjpe_J4GaCc_EQNqLwz_MZop3LHeCZGNJNLI"
+    }
+
+    component.form.setValue({
+      email: 'admi@gmail.com',
+      password: 'admi123'
+    });
+    spyOn(authApiService, 'loginAuth').and.returnValue(of(response))
+    component.login()
+    fixture.detectChanges();
+    fixture.whenStable();
+    expect(mockRouter.navigate).toHaveBeenCalled()
+    expect(authApiService.loginAuth).toHaveBeenCalled()
+
+  })
+  it('Should be login Error', () => {
+    component.form.setValue({
+      email: 'admi@gmail.com',
+      password: 'admi123'
+    });
+    spyOn(authApiService, 'loginAuth').and.returnValue(throwError('error login'))
+    component.login()
+    fixture.detectChanges();
+    fixture.whenStable()
     expect(authApiService.loginAuth).toHaveBeenCalled()
 
   })
